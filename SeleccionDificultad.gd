d775cde9c5fb470b08a2d3581e4356ef2bfb178f
lukovski <lucasottonellodolio@gmail.com>
2025-07-04 10:55:30 -0300

    a ver

--- SeleccionDificultad.gd
+++ SeleccionDificultad.gd
@@ -6,9 +6,18 @@ signal dificultad_seleccionada(nivel: String)
 @onready var dificil_btn = $VBoxContainer/Dificil
 
 func _ready():
-	facil_btn.pressed.connect(func(): _seleccionar("facil"))
-	dificil_btn.pressed.connect(func(): _seleccionar("dificil"))
+	facil_btn.pressed.connect(_on_facil_pressed)
+	dificil_btn.pressed.connect(_on_dificil_pressed)
+
+
+func _on_facil_pressed():
+	_seleccionar("facil")
+
+func _on_dificil_pressed():
+	_seleccionar("dificil")
 
 func _seleccionar(nivel: String):
+	# Guardamos en Global y persistencia
+	Global.set_dificultad(nivel)
 	emit_signal("dificultad_seleccionada", nivel)
 	queue_free()
